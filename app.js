f6da8b5d338ddd48a0603b618ba5cef9e83df794
venkateswaransankaran <venkateswaran.sankaran@gmail.com>
2021-06-17 15:47:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing out the first Venky Project'));
+app.get('/', (req, res) => res.send('Full Pipeline Test Venky'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
